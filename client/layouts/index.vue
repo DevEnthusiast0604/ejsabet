ebcbcdb4e4b10aa9e7659ec41fdd5d7cb3ba7237
TopDev <nguyen91.trai@outlook.com>
2024-05-01 15:19:10 +1100

    default layout page

---
+++ client/layouts/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<nuxt />
+</template>
+
+<script>
+import InactivityCheck from '@/mixins/InactivityCheck.js'
+export default {
+    mixins: [InactivityCheck],
+    head() {
+        return {
+            htmlAttrs: {
+                'data-layout-mode': 'index'
+            },
+        };
+    },
+}
+</script>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Nunito:400,700');
+</style>
+<style src="../assets/sass/bootstrap4.5.2.css" lang="css" scoped></style>
+<style src="../assets/sass/index.scss" lang="scss"></style>
+
