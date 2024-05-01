da554c92865e9d01b31bdd7a5031a296144773f2
TopDev <nguyen91.trai@outlook.com>
2024-05-02 10:34:55 +1100

    lang store

---
+++ client/store/lang.js
@@ -0,0 +1,32 @@
+import Cookies from 'js-cookie'
+
+// state
+export const state = () => ({
+  locale: process.env.appLocale,
+  locales: {
+    en: 'English',
+    es: 'Español'
+  }
+})
+
+// getters
+export const getters = {
+  locale: state => state.locale,
+  locales: state => state.locales
+}
+
+// mutations
+export const mutations = {
+  SET_LOCALE (state, { locale }) {
+    state.locale = locale
+  }
+}
+
+// actions
+export const actions = {
+  setLocale ({ commit }, { locale }) {
+    commit('SET_LOCALE', { locale })
+
+    Cookies.set('locale', locale, { expires: 365 })
+  }
+}
